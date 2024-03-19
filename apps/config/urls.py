8e6b57e73e0f9175fa1d1d8eaa509a61367a2f06
TK Global Technology <157607545+lucas-charisma@users.noreply.github.com>
2024-03-18 23:42:40 -0300

    update

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -7,7 +7,7 @@ app_name='config'
 
 urlpatterns =[
     path('grupos-whatsapp/', views.get_groups_whatsapp, name='grupos_whatsapp'),
-    path('my-grupos-whatsapp/', views.get_my_groups_whatsapp, name='get_my_groups_whatsapp123'),
+    path('my-grupos-whatsapp/', views.get_my_groups_whatsapp, name='get_my_groups_whatsapp'),
     path('salvar-groupo-whatsapp/', views.save_selected_groups, name='save_selected_groups'),
     path('deletar-groupo-whatsapp/<int:pk>', views.delete_group, name='delete_group'),
 ]
